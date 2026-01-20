97bd40bd82a58588e5f350cbab06b9442ef699c2
Платон <69915796+CHERNOBOGER@users.noreply.github.com>
2026-01-20 16:53:26 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,23 @@
 rootProject.name = "MacRosNik"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+toolchainManagement {
+    jvm {
+        javaRepositories {
+            repository("foojay") {
+                resolverClass.set(
+                    org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java
+                )
+            }
+        }
+    }
+}
